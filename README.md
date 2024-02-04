b53c8d2b55382ea6beeb85ba29a123fed27af36f
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-02-04 22:14:07 +0200

    Inastall express and nodemon.Config package-json

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Wizard-creatures
 JS Backend
+1.Init project and structure
+2.Setup developer environment(Made a file constants.js);
+3.Install express and nodemon.Made a run script "start" with route src/index.js.Same for the "main".
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello');
+const express = require('express');
+const { PORT } = require('./constants.js');
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello home page!');
+});
+
+app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}...`));
