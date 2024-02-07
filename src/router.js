12dbd39aed06a052e9b58f6ce9aa8b27b56c093d
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-02-07 20:56:27 +0200

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const homeControler = require('./controllers/homeController')
 const userControler = require('./controllers/userController')
-//TODO add endpoints with controllers her...
+
 
 router.use(homeControler);
 router.use('/users', userControler);
