2f2a79dd7892658f51955092789fd25991422292
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-02-07 20:56:30 +0200

    Update userService.js

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,9 +1,32 @@
 const User = require("../models/User");
+const bcrypt = require('bcrypt');
 const jwt = require('../lib/jwt');
 const { SECRET } = require('../constants');
 
-exports.register = (userData) => User.create(userData);
-const bcrypt = require('bcrypt');
+async function validatePassword(password, userPassword) {
+    const isValid = await bcrypt.compare(password, userPassword);
+    if (!isValid) {
+        throw new Error('Invalid email or password');
+    }
+}
+
+async function getToken(user) {
+
+    const payload = { _id: user._id, email: user.email };
+    const token = await jwt.sign(payload, SECRET, { expiresIn: '3d' });
+
+
+    return token;
+}
+
+exports.register = async (userData) => {
+    const { password } = userData;
+    const user = await User.create(userData);
+    await validatePassword(password, user.password);
+    const token = await getToken(user);
+    return token;
+
+}
 
 exports.login = async (email, password) => {
     const user = await User.findOne({ email });
@@ -11,14 +34,8 @@ exports.login = async (email, password) => {
     if (!user) {
         throw new Error('Invalid email or password');
     }
-
-    const isValid = await bcrypt.compare(password, user.password);
-    if (!isValid) {
-        throw new Error('Invalid email or password');
-    }
-    const payload = { _id: user._id, email: user.email };
-    const token = await jwt.sign(payload, SECRET, { expiresIn: '3d' });
-
-
+    await validatePassword(password, user.password);
+    const token = await getToken(user);
     return token;
+
 };
